87d48303682449c0fe38bc7e5f8717c491eaadc5
Haitham Alsharif <mrhaithamalsharif@gmail.com>
2019-01-29 12:39:13 +0300

    no errors - for PC

--- acme/routes/views/product.js
+++ acme/routes/views/product.js
@@ -1,16 +1,29 @@
 var keystone = require('keystone');
 
-exports = module.exports = function( req, res) {
-    var view = new keystone.View(req,res);
-    var locals = res.locals; // passing in our local variables
+exports = module.exports = function(req, res) {
+  var view = new keystone.View(req, res);
+  var locals = res.locals;
 
-    // set locals 
+  // Set locals
+  locals.section = 'store';
+  locals.filters = {
+    product: req.params.product
+  }
+  locals.data = {
+    products:[]
+  }
 
-    locals.section = 'store' // this is for the navbar
+view.on('init', function(next){
+  var q = keystone.list('Product').model.findOne({
+    slug: locals.filters.product
+  });
 
-    //Load products 
+  q.exec(function(err, result){
+    locals.data.product = result;
+    next(err);
+  });
+});
 
-    view.query('products', keystone.list('Product').model().find()); // pass the list made in the Product model, to the template ? that is products
-
-    view.render('products')
+  // Render View
+  view.render('product');
 }
